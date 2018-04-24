1ada42d694fae8563018ae03b884c98025956db0
DethierY <https://github.com/DethierY/Algo.git>
2018-04-24 15:46:02 +0200

    ajout du cross origin

--- src/main/java/filrouge/gedesaft/controller/VehiculeController.java
+++ src/main/java/filrouge/gedesaft/controller/VehiculeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import filrouge.gedesaft.model.Vehicule;
 import filrouge.gedesaft.service.VehiculeService;
 
 // Classe gérant la liaison entre la base données, le serveur (cette application Spring Boot) et le client (application angular)
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/vehicule")
 public class VehiculeController {
